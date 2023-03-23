718bcd5cc9e17a65f139ef60e552e53860fc2a92
bathapudi <prasanna.bethapudi@smartdataglobal.in>
2023-03-23 15:54:04 +0530

    working on main page ui

--- EdgePulse.Superstore.Main/MainWindow.xaml.vb
+++ EdgePulse.Superstore.Main/MainWindow.xaml.vb
@@ -2,4 +2,8 @@
     Private Sub mainWin_Loaded(sender As Object, e As RoutedEventArgs) Handles mainWin.Loaded
         mainWin.Content = New CleintMaintenance()
     End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As RoutedEventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
 End Class
