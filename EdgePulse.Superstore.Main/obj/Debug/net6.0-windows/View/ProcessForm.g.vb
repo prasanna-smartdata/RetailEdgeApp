c98e47105347add24527fe3d48a91c00b35a679a
bharathdumbala <bharathdumbala@outlook.com>
2023-03-31 17:44:46 +0530

    Checkbox changes

--- EdgePulse.Superstore.Main/obj/Debug/net6.0-windows/View/ProcessForm.g.vb
+++ EdgePulse.Superstore.Main/obj/Debug/net6.0-windows/View/ProcessForm.g.vb
@@ -50,7 +50,7 @@ Partial Public Class ProcessForm
     '''InitializeComponent
     '''</summary>
     <System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-     System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")>  _
+     System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")>  _
     Public Sub InitializeComponent() Implements System.Windows.Markup.IComponentConnector.InitializeComponent
         If _contentLoaded Then
             Return
@@ -65,7 +65,7 @@ Partial Public Class ProcessForm
     End Sub
     
     <System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-     System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0"),  _
+     System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0"),  _
      System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never),  _
      System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes"),  _
      System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity"),  _
